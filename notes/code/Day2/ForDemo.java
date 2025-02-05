c45fa1ce14df4a9caf06bb5134014cec03a03e9d
ic3vala <105295294+ic3vala@users.noreply.github.com>
2025-02-05 16:12:10 +0800

    Create ForDemo.java

---
+++ notes/code/Day2/ForDemo.java
@@ -0,0 +1,10 @@
+public class ForDemo {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + (i * i) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
