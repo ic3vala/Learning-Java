a256246cea8a6e2500197ff659636d9440e2827f
ic3vala <105295294+ic3vala@users.noreply.github.com>
2025-02-05 21:30:25 +0800

    Create ArrayDemo1.java

---
+++ notes/code/Day3/ArrayDemo1.java
@@ -0,0 +1,29 @@
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+        int[] arrays = {1,2,3,4,5};
+
+        //打印全部的数组元素
+        for(int i=0;i<arrays.length;i++){
+            System.out.println(arrays[i]);
+        }
+        System.out.println("===============");
+
+        //计算所有元素的和
+        int sum=0;
+        for(int i=0;i<arrays.length;i++){
+            sum+=arrays[i];
+        }
+        System.out.println("sum="+sum);
+        System.out.println("===============");
+        //查找最大元素
+        int max = arrays[0];
+
+        for(int i=0;i<arrays.length;i++){
+            if(arrays[i]>max){
+                max=arrays[i];
+            }
+        }
+
+        System.out.println("max="+max);
+    }
+}
