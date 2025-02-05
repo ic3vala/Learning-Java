62662854838a2de077bef4e2d619078ba849b4d1
ic3vala <105295294+ic3vala@users.noreply.github.com>
2025-02-05 20:57:15 +0800

    Create ArrayDemo.java

---
+++ notes/code/Day3/ArrayDemo.java
@@ -0,0 +1,18 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] nums; //2.声明一个数组
+        nums = new int[10]; //3.创建一个数组
+
+        //3.给数组元素赋值
+        for (int i = 0; i < 10; i++) {
+            nums[i] = i+1;
+        }
+
+        //计算所有元素的和
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        System.out.println("总和为："+sum);
+    }
+}
